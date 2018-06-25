a13242b158a3339044955a1ff3624d683c2a3da5
OlejukM <olejukmaslo@gmail.com>
2018-06-26 01:58:49 +0300

    app

--- jss.js
+++ jss.js
@@ -25,52 +25,12 @@ class Art {
 
 const art = new Art();
 
-art.add('2017-04-25', 2, 'USD', 'Jogurt');
-art.add('2017-04-25', 3, 'USD', 'French fries');
-art.add('2017-04-27', 4.75, 'USD', 'Beer');
+art.add('2017-04-25', 2, 'USD', 'Jogurt'); // add expenses to list
+art.add('2017-04-25', 3, 'USD', 'French fries'); // add expenses to list
+art.add('2017-04-27', 4.75, 'USD', 'Beer'); // add expenses to list
 
-art.clear('2017-04-27');
+art.clear('2017-04-27'); // removes all expenses for specified date,
 
-console.log(art.list());
+console.log(art.list()); //output expenses list
 
-console.log(art.total());
-
-
-// class Art {
-//     constructor() {
-//         this.values = Array();
-//         this.totals = Object.create(null);
-//     }
-//     add(date, amount, currency, product) {
-//         this.totals[currency] = this.totals[currency] || 0;
-//         this.totals[currency] += amount;
-//         this.values.push([date, amount, currency, product]); //.toString());
-//     }
-//     list() {
-//         return this.values;
-//     }
-//     clear(date) {
-//         var amount, currency;
-//         for (let i = this.values.length - 1; i >= 0; --i) {
-//             if (this.values[i][0] == date) {
-//                 [, amount, currency] = this.values.splice(i, 1)[0]
-//                 this.totals[currency] -= amount;
-//             }
-//         }
-//     }
-//     total() {
-//         return this.totals;
-//     }
-// }
-
-// const art = new Art();
-
-// art.add('2017-04-25', 2, 'USD', 'Jogurt');
-// art.add('2017-04-25', 3, 'USD', 'French fries');
-// art.add('2017-04-27', 4.75, 'USD', 'Beer');
-
-// art.clear('2017-04-27');
-
-// console.log(art.list());
-
-// console.log(art.total());
+console.log(art.total()); //output expenses total
